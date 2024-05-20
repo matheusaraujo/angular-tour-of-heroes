4a054b7ebd82327793ac79d63e88e5370d215c76
Matheus Araujo <matheus.saraujo@gmail.com>
2024-05-20 18:23:17 +0200

    chore: toh-pt2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
-import { Hero } from "../hero";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+
+import { HEROES } from "../mock-heroes";
+import { Hero } from "../hero";
 
 @Component({
   selector: "app-heroes",
@@ -11,8 +13,10 @@ import { FormsModule } from "@angular/forms";
   styleUrl: "./heroes.component.css",
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm",
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
