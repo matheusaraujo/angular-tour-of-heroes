9421e4c0d6da9408a022e96e51be61c67572e318
Matheus Araujo <matheus.saraujo@gmail.com>
2024-05-20 19:09:26 +0200

    chore: toh-pt5 - a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { RouterLink, RouterModule, RouterOutlet } from "@angular/router";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { MessagesComponent } from "./messages/messages.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, MessagesComponent],
+  imports: [RouterOutlet, HeroesComponent, MessagesComponent, RouterModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
