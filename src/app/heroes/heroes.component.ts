78f2a22472a914a0df31179c991517ef24268dc6
Matheus Araujo <matheus.saraujo@gmail.com>
2024-05-20 18:51:40 +0200

    chore: toh-pt4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HeroService } from "../hero.service";
 import { Hero } from "../hero";
 import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: "app-heroes",
@@ -13,17 +14,22 @@ import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
   styleUrl: "./heroes.component.css",
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService,
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
